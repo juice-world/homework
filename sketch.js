376151f5e20ba5d4a8d181a4db3c118fd0e7a816
juice-world <87857753+juice-world@users.noreply.github.com>
2021-08-18 13:28:54 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,87 +1,204 @@
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
-const Body = Matter.Body;
 const Constraint = Matter.Constraint;
 
-var roof;
-var ball;
-var ball2;
-//Create multiple bobs, mulitple ropes varibale here
+var engine, world;
+var canvas;
+var palyer, playerBase, playerArcher;
+var computer, computerBase, computerArcher;
+var playerArrows = [];
+var computerArrows = [];
+//
+//Declare the varibales to add 3 life for player and computerplayer
 
+function preload() {
+  //Load Image of background
+
+
+}
 
 function setup() {
-	createCanvas(800, 600);
-	rectMode(CENTER);
+  canvas = createCanvas(windowWidth, windowHeight);
 
-	engine = Engine.create();
-	world = engine.world;
+  engine = Engine.create();
+  world = engine.world;
 
-	var roof_options={
-		isStatic:true			
-	}
+  playerBase = new PlayerBase(300, random(450, height - 300), 180, 150);
+  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.body.position.y - 180,
+    120,
+    120
+  );
 
-	roof = Bodies.rectangle(400,100,230,20,roof_options);
-    World.add(world,roof);
+  computerBase = new ComputerBase(
+    width - 300,
+    random(450, height - 300),
+    180,
+    150
+  );
+  computer = new Computer(
+    width - 280,
+    computerBase.body.position.y - 153,
+    50,
+    180
+  );
 
-	Engine.run(engine);
-	
-    ball = Bodies.circle(200,50,10,ball_options);
-  World.add(world,ball);
-
-  
-  ball2 = Bodies.circle(100,50,10,ball_options);
-  World.add(world,ball2);
-  
-  
-  con = Matter.Constraint.create({
-          pointA:{x:200,y:20},
-          bodyB:ball,
-          pointB:{x:0,y:0},
-          length:100,
-          stiffness:0.1
-        });
-  
-      World.add(world,con);
-      
-  
-  
-  
-  
-  con2 = Matter.Constraint.create({
-    bodyA:ball,
-    pointA:{
-      x:0,y:0
-    },
-    bodyB:ball2,
-    pointB:{
-      x:0,y:0
-    },
-    length:100,
-    stiffness:0.1
-  });
-
-World.add(world,con2);
-
-
-
-rectMode(CENTER);
-ellipseMode(RADIUS);
-
+  computerArcher = new ComputerArcher(
+    width - 350,
+    computerBase.body.position.y - 180,
+    120,
+    120
+  );
+  handleComputerArcher();
 }
 
 function draw() {
-  rectMode(CENTER);
-  background("#99004d");
+  background("./assets/background.gif");
 
-  rect(roof.position.x,roof.position.y,230,20);
+  Engine.update(engine);
 
-  //call display() to show ropes here
   
-  
-  //create ellipse shape for multiple bobs here
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
 
+  for (var i = 0; i < playerArrows.length; i++) {
+    showArrows(i, playerArrows);
+  }
+
+  playerBase.display();
+  player.display();
+  
+  playerArcher.display();
+  handlePlayerArrowCollision();
+
+  for (var i = 0; i < computerArrows.length; i++) {
+    showArrows(i, computerArrows);
+  }
+  //call Player.life and computerplayer.life
+
+
+  computerBase.display();
+  computer.display();
+  
+  computerArcher.display();
+  handleComputerArrowCollision();
+}
+
+function keyPressed() {
+  if (keyCode === 32) {
+    var posX = playerArcher.body.position.x;
+    var posY = playerArcher.body.position.y;
+    var angle = playerArcher.body.angle;
+
+    var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+
+    arrow.trajectory = [];
+    Matter.Body.setAngle(arrow.body, angle);
+    playerArrows.push(arrow);
+  }
+}
+
+function keyReleased() {
+  if (keyCode === 32) {
+    if (playerArrows.length) {
+      var angle = playerArcher.body.angle;
+      playerArrows[playerArrows.length - 1].shoot(angle);
+    }
+  }
+}
+
+function showArrows(index, arrows) {
+  arrows[index].display();
  
 }
 
-//Write keyPressed function and apply force on pressing up_arrow key on the first bob.
+function handleComputerArcher() {
+  if (!computerArcher.collapse && !playerArcher.collapse) {
+    setTimeout(() => {
+      var pos = computerArcher.body.position;
+      var angle = computerArcher.body.angle;
+      var moves = ["UP", "DOWN"];
+      var move = random(moves);
+      var angleValue;
+
+      if (move === "UP") {
+        angleValue = 0.1;
+      } else {
+        angleValue = -0.1;
+      }
+      angle += angleValue;
+
+      var arrow = new ComputerArrow(pos.x, pos.y, 100, 10, angle);
+
+      Matter.Body.setAngle(computerArcher.body, angle);
+      Matter.Body.setAngle(computerArcher.body, angle);
+
+      computerArrows.push(arrow);
+      setTimeout(() => {
+        computerArrows[computerArrows.length - 1].shoot(angle);
+      }, 100);
+
+      handleComputerArcher();
+    }, 2000);
+  }
+}
+
+function handlePlayerArrowCollision() {
+  for (var i = 0; i < playerArrows.length; i++) {
+    var baseCollision = Matter.SAT.collides(
+      playerArrows[i].body,
+      computerBase.body
+    );
+
+    var archerCollision = Matter.SAT.collides(
+      playerArrows[i].body,
+      computerArcher.body
+    );
+
+    var computerCollision = Matter.SAT.collides(
+      playerArrows[i].body,
+      computer.body
+    );
+
+    if (
+      baseCollision.collided ||
+      archerCollision.collided ||
+      computerCollision.collided
+    ) {
+      console.log("Player Arrow Collided")
+    }
+  }
+}
+
+function handleComputerArrowCollision() {
+  for (var i = 0; i < computerArrows.length; i++) {
+    var baseCollision = Matter.SAT.collides(
+      computerArrows[i].body,
+      playerBase.body
+    );
+
+    var archerCollision = Matter.SAT.collides(
+      computerArrows[i].body,
+      playerArcher.body
+    );
+
+    var playerCollision = Matter.SAT.collides(
+      computerArrows[i].body,
+      player.body
+    );
+
+    if (
+      baseCollision.collided ||
+      archerCollision.collided ||
+      playerCollision.collided
+    )
+    {
+      console.log("Computer Arrow Collided")
+    }
+  }
+}
